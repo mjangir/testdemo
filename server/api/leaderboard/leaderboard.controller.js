d67712ad772680368f77a21145d6135607968965
Manish <mjangir70@gmail.com>
2018-03-12 22:43:39 +0530

    change

--- server/api/leaderboard/leaderboard.controller.js
+++ server/api/leaderboard/leaderboard.controller.js
@@ -132,8 +132,21 @@ const getLeaderboardScore = function(record, type) {
 }
 
 const getHumanDuration = function(seconds) {
-  var t = convertSecondsToCounterTime(seconds);
+  var t = msToTime(seconds);
   return t.hours + ':' + t.minutes + ':' + t.seconds;
+}
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration%1000)/100)
+      , seconds = parseInt((duration/1000)%60)
+      , minutes = parseInt((duration/(1000*60))%60)
+      , hours = parseInt((duration/(1000*60*60))%24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
 
 export default {
